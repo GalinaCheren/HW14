a4481366cacc9cb52e9358d47eab698742d6f8f9
GalinaCheren <galinacheren8@gmail.com>
2022-10-23 22:34:14 +0300

    Home Work 14 Objects

---
+++ index.js
@@ -0,0 +1,31 @@
+const store = {
+  name: "PRO'beauty",
+  address: {
+    country: "Ukraine",
+    city: "Odesa",
+    street: "Deribasivska str.",
+    houseNumber: 18,
+  },
+};
+
+store.clients = {};
+store.clients.MikhailHitko = {
+  quantity: 10,
+  bonuses: 5,
+};
+store.clients.OleksandraHorbenko = {
+  quantity: 20,
+  bonuses: 10,
+};
+store.clients.OleksiiYelanskiy = {
+  quantity: 25,
+  bonuses: 10,
+};
+store.clients.OlenaDemchyk = {
+  quantity: 5,
+  bonuses: 0,
+};
+store.clients.OlhaNevzghodova = {
+  quantity: 30,
+  bonuses: 15,
+};
